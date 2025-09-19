5f8725fa9b8a81480f2bdd59dba4d02eb4b87455
Héric <hericsilveira2010@outlook.com.br>
2025-09-18 23:13:10 -0300

    fix

--- main.py
+++ main.py
@@ -5,16 +5,5 @@ import uvicorn, time
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return Response()
-
-def keepAlive():
-    while True:
-        with request.urlopen("https://granaflow-c6wo.onrender.com/") as response:
-            ...
-        time.sleep(60)
-
 if __name__ == "__main__":
-    Thread(target=keepAlive, daemon=True).start()
     uvicorn.run(app="main:app", host='0.0.0.0', port=8000)
